c3f67a1eda20ff4d974f52a240f59956a4f7e46e
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-11-24 19:09:30 -0500

    class exercice made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sesion05';
+  //  declaration variables
+
+  // public mensaje1 = "Sueldo mayor al sueldo minimo"
+  // public mensaje2 = "Sueldo menor al sueldo minimo"
+  // public sueldo: Number = 3000
+
+  // public empleados: string[] = ["Juan", "Pedro", "Julio", "Carlos", "Luis", "Rosa", "Maria", "Carmen", "Sara", "Sandra"]
+
+  public numeros: any[] = [12, 25, 234, 56, 24, 45, 23, 51, 6, 12, 64, 93, 47];
+
+
+
 }
